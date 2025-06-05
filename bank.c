6ac4396224f7fcc2619c75b9284e70a0b39b4b03
Sveshnikov <esbarros@proton.me>
2025-06-04 21:45:06 -0300

    Update bank.c

--- bank.c
+++ bank.c
@@ -1,65 +1,12 @@
 #include <stdio.h>
-
-void selectOption(int *option);
-void printBalance(float *balance);
-void askBalance(float *balance);
-void printMenu();
-void deposit(float *balance);
-void withdraw(float *balance);
-void loan();
-void exitProgram();
-
-void selectOption(int *option) {
-    printf("Select an option: ");
-    scanf("%d", option);
-}
-
-void printBalance(float *balance) {
-    printf("\nYour balance: $%.2f\n\n", *balance);
-}
-
-void askBalance(float *balance) {
-    printf("What is your balance? ");
-    scanf("%f", balance);
-    printf("Welcome to the bank!\n\n");
-}
-
-void printMenu() {
-    const char *arr[] = {"Deposit", "Withdraw", "Check Balance", "Loan", "Exit"};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Available options:\n\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d. %s\n", i + 1, arr[i]);
-    }
-}
-
-void deposit(float *balance) {
-    float depositValue;
-    printf("How much do you want to deposit? ");
-    scanf("%f", &depositValue);
-    *balance += depositValue;
-    printf("New balance: $%.2f\n", *balance);
-}
-
-void withdraw(float *balance) {
-    float withdrawValue;
-    printf("How much do you want to withdraw? ");
-    scanf("%f", &withdrawValue);
-    *balance -= withdrawValue;
-    printf("New balance: $%.2f\n", *balance);
-}
-
-void loan() {
-    float loanValue;
-    printf("What is the value of your loan? ");
-    scanf("%f", &loanValue);
-    printf("Loan request for $%.2f received.\n", loanValue);
-}
-
-void exitProgram() {
-    printf("Thank you for using the bank!\n");
-}
+#include "ask_balance.h"
+#include "deposit.h"
+#include "withdraw.h"
+#include "loan.h"
+#include "exit_program.h"
+#include "print_menu.h"
+#include "print_balance.h"
+#include "select_option.h"
 
 int main(void) {
     float balance;
